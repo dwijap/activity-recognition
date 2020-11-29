1d547ade95f52ccbf7db5ffbac1f1b6e84183929
Dwija <dwijaparikh@gmail.com>
2020-11-28 22:54:38 -0600

    Added gender

--- project4322.R
+++ project4322.R
@@ -1,17 +1,27 @@
 #READING THE DATA
+library(tidyverse)
 knitr::opts_chunk$set(echo = TRUE, cache = TRUE, comment = "")
-files.1 <- list.files('C:/Users/dwija/Documents/UH/FALL 2020/MATH 4322/project/Datasets_Healthy_Older_People/S1_Dataset/')
-files.2 <- list.files('C:/Users/dwija/Documents/UH/FALL 2020/MATH 4322/project/Datasets_Healthy_Older_People/S2_Dataset/')
-files.1 <- paste0('C:/Users/dwija/Documents/UH/FALL 2020/MATH 4322/project/Datasets_Healthy_Older_People/S1_Dataset/', files.1[-length(files.1)])  # do not read the readme files
-files.2 <- paste0('C:/Users/dwija/Documents/UH/FALL 2020/MATH 4322/project/Datasets_Healthy_Older_People/S2_Dataset/', files.2[-length(files.2)])
+setwd("C:/Users/dwija/Documents/UH/FALL 2020/MATH 4322/project/Datasets_Healthy_Older_People")
 
-data.1 <- do.call(rbind, lapply(files.1, function(x) read.csv(file=x, header = F)))
-data.2 <- do.call(rbind, lapply(files.2, function(x) read.csv(file=x, header = F)))
+files.1 <- list.files('S1_Dataset/')
+files.2 <- list.files('S2_Dataset/')
 
+files.1 <- paste0('S1_Dataset/', files.1[-length(files.1)])  # do not read the readme files
+files.2 <- paste0('S2_Dataset/', files.2[-length(files.2)])
+
+ReadData <- function(file_name) {
+  
+  data <- read_csv(file_name, col_names = c("Time", "Acceleration.front", "Acceleration.vert","Acceleration.lat","Antenna","RSSI","Phase","Frequency","Activity"))
+  # Adding columns by parsing filenames and directories
+  file.name <- str_split(file_name, "/", simplify = TRUE, n = 5)
+  data['Gender'] <- as.integer(ifelse(str_sub(file.name[5], -1) == "F", 0, 1))
+  #data['Room ID'] <- as.integer(ifelse(str_sub(file.name[4], 1,2) == "S1", 1, 2))
+  
+  return(data)
+}
+data.1 <- do.call(rbind, lapply(files.1, ReadData))
+data.2 <- do.call(rbind, lapply(files.2, ReadData))
 data <- rbind(data.1, data.2)
-colnames(data) <- c("Time", "Acceleration.front", "Acceleration.vert","Acceleration.lat",
-                    "Antenna","RSSI","Phase","Frequency","Activity")
-
 
 head(data)
 data$Activity <- factor(data$Activity)
